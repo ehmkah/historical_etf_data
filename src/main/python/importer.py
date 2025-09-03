97eca244ae934e9d2fa3ecde61d23521026e45b4
Michael Krausse <michael.krausse@directbox.com>
2025-09-03 21:18:43 +0200

    database added : extract stocks

--- src/main/python/importer.py
+++ src/main/python/importer.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import os
+import uuid
 
 def readCountries(filename):
     """Read and print the contents of a CSV file."""
@@ -12,7 +13,6 @@ def readCountries(filename):
 
     with open(filename, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-        print(f"--- Reading '{filename}' ---")
         for row in reader:
             val = row['Standort']
             stock = row['Anlageklasse']
@@ -23,6 +23,31 @@ def readCountries(filename):
 
     for countryName in sorted(values):
         print(f"INSERT INTO countries (countryname) VALUES ('{countryName}');");
+
+def readStocks(filename):
+    """Read and print the contents of a CSV file."""
+    if not os.path.isfile(filename):
+        print(f"Error: File '{filename}' not found.")
+        sys.exit(1)
+
+    values = []
+
+    with open(filename, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            countryName = row['Standort']
+            stockName = row['Name']
+            anlageKlasse = row['Anlageklasse']
+            if countryName is not None and anlageKlasse == 'Aktien':
+                countryName = countryName.strip()
+                if countryName:
+                    values.append({"stockName" : stockName, "countryName" : countryName})
+
+    for stock in values:
+        stockName = stock['stockName']
+        countryName = stock['countryName']
+        isin = uuid.uuid4()
+        print(f"INSERT INTO stocks (stock_name, country_id, isin) VALUES ('{stockName}', (select id from countries where countryname='{countryName}'), '{isin}');")
 
 if __name__ == "__main__":
     # Check for filename argument
@@ -35,3 +60,5 @@ if __name__ == "__main__":
     match type.lower():
             case "countries":
                 readCountries(csv_file)
+            case "stocks":
+                readStocks(csv_file)
